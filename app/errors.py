451e6367aa25f7ed6950173e7d7d2bced6e6fb4b
andrey <serdyukov963@gmail.com>
2025-03-31 03:45:57 +0300

    gave more description to errors

--- app/errors.py
+++ app/errors.py
@@ -4,14 +4,14 @@ class VaccineError(Exception):
 
 class NotVaccinatedError(VaccineError):
     def __str__(self):
-        return f"NotVaccinatedError"
+        return f"Visitor is not vaccinated and cannot enter the cafe"
 
 
 class OutdatedVaccineError(VaccineError):
     def __str__(self):
-        return f"OutdatedVaccineError"
+        return f"Visitors vaccine is outdated"
 
 
 class NotWearingMaskError(Exception):
     def __str__(self):
-        return f"NotWearingMaskError"
+        return f"Visitor is not wearing mask and cannot enter the cafe"
